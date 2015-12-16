4465df027db096efe40945e83a2a65afe9688501
Zech Dzegede <zkdzegede@gmail.com>
2015-12-17 01:31:33 +0900

    fix init to use new appModule.run

--- src/init.lua
+++ src/init.lua
@@ -2,6 +2,6 @@ print("init: heap "..node.heap())
 print("Wait 3 seconds to start app to allow PANIC loop override")
 tmr.alarm(0, 3000, 1, function()
     local appModule = require "appModule"
-    appModule.init()
+    appModule.run()
     tmr.stop(0);
 end)
