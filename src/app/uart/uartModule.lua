c55c173cdd235e72c71221cc65d254b60117bc71
Zech Dzegede <zkdzegede@gmail.com>
2015-12-18 22:55:34 +0900

    add mo’ mock data

--- src/app/uart/uartModule.lua
+++ src/app/uart/uartModule.lua
@@ -8,6 +8,19 @@ local fileCount = 0
 local fileOpen = false
 local maxFiles = 2
 
+function uartModule.init()
+    package.loaded[module]=nil
+    
+    local wifiModule = require "wifiModule"
+    wifiModule.init()
+    local loaded = wifiModule.loadWifiSettings()
+    if (loaded) then
+        wifiModule.setupWifi(uartModule.wifiCallback)
+    else
+        print("Could not load wifi settings")
+    end
+end
+
 function uartModule.wifiCallback(success)
     if (success) then
         -- wifi connected
@@ -24,8 +37,8 @@ function uartModule.streamingCallback(startStreaming, c)
         print("start stream")
         tmr.alarm(2, 100, 1, function()
             -- TODO turn into uart callback
-            local line = getLineFromFile()
-            count = clientModule.sendSample(getFakeChannelData(line))
+            local line = tempGetData()
+            count = clientModule.sendSample(tempFormatData(line))
         end)
      else
         print("stop stream")
@@ -33,7 +46,7 @@ function uartModule.streamingCallback(startStreaming, c)
      end
 end
 
-function getLineFromFile() 
+function tempGetData() 
     if (not fileOpen) then
         local fileToOpen = "testData"..(fileCount%maxFiles+1)..".txt"
         print("open "..fileToOpen)
@@ -55,32 +68,18 @@ function getLineFromFile()
     end
 end
 
-function uartModule.init()
-    package.loaded[module]=nil
-    
-    local wifiModule = require "wifiModule"
-    wifiModule.init()
-    local loaded = wifiModule.loadWifiSettings()
-    if (loaded) then
-        wifiModule.setupWifi(uartModule.wifiCallback)
-    else
-        print("Could not load wifi settings")
-    end
-end
-
-function getFakeChannelData(line)
+function tempFormatData(line)
     return {
-              Battery=12,
-              Distance=20*count,
-              LapCount=math.floor(fileCount/maxFiles),
-              CurrentLap=count/60,
-              Latitude=line[2],
-              Longitude=line[1],
-              Sector=0,
-              Speed=20*math.random(),
-              Time=count/60,
-              PredTime=count/60
-              }
+            Battery=12,
+            Time=count/60,
+            Longitude=line[1],
+            Latitude=line[2],
+            Distance=line[3],
+            Speed=line[4],
+            LapCount=math.floor(fileCount/maxFiles),
+            CurrentLap=count/60,
+            PredTime=count/60
+           }
 end
 
 return uartModule
