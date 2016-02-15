42df97549e8962798ae4969eb828f7c0794f6a4a
Zech Dzegede <zkdzegede@gmail.com>
2016-02-15 23:30:46 +0900

    server shows logs instead of handling lua input via telnet

--- src/app/wifi/serverModule.lua
+++ src/app/wifi/serverModule.lua
@@ -8,22 +8,51 @@ function serverModule.init()
         print("createServer")
         s=net.createServer(net.TCP,180) 
     end
-    s:listen(2323,function(c) 
-        function s_output(str) 
-          if(c~=nil) 
-            then c:send(str) 
-          end 
-        end 
-        node.output(s_output, 0)   
-        -- re-direct output to function s_ouput.
-        c:on("receive",function(c,l) 
-          node.input(l)           
-          --like pcall(loadstring(l)), support multiple separate lines
-        end) 
-        c:on("disconnection",function(c) 
-          node.output(nil)        
-          --unregist redirect output function, output goes to serial
+    s:listen(80,function(c) 
+        c:on("disconnection",function(c)  
+            print("c disconnection")
         end)
+     
+        c:on("receive", function(c, request)
+            local buf = ""
+            function esp_update(request, postparse)
+                mcu_do=string.sub(request,postparse[2]+1,#request)
+                if mcu_do == "Clear" then
+                    log.clear()
+                end
+
+                if mcu_do == "Remove+Init" then
+                    file.remove("init.lua")
+                    file.remove("init.lc")
+                    node.restart()
+                end
+            end
+            postparse={string.find(request,"mcu_do=")}
+            if postparse[2]~=nil then 
+                esp_update(request, postparse)
+            end
+            buf = buf.."HTTP/1.1 200 OK\r\n\r\n"
+            buf = buf.."<html><body>"
+            buf = buf.."<h1> PMT Racing Log</h1>\n"
+            buf = buf.."<form action='' method='POST'>\n"
+            buf = buf.."<input type='submit' name='mcu_do' value='Clear'>\n"
+            buf = buf.."<input type='submit' name='mcu_do' value='Refresh'>\n"
+            buf = buf.."<input type='submit' name='mcu_do' value='Remove Init'>\n"
+            buf = buf.."<p>"
+            local logValue = log.read("d", 100)
+            if (logValue) then
+                for x = 1, #logValue do
+                    buf = buf..logValue[x].."<br>"
+                    tmr.wdclr()
+                end
+            else
+                buf = buf.."No log"
+            end
+            buf = buf.."</p>"
+            buf = buf.."</body></html>"
+            c:send(buf)
+            c:close()
+        end) 
     end)
 end
 
